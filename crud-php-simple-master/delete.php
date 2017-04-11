878a0e55d6b3d4a89d9936798a8c91bc9e6fccc3
Moxxe <moxxew@gmail.com>
2017-04-11 15:11:16 +0500

    admin panel v7.5

--- crud-php-simple-master/delete.php
+++ crud-php-simple-master/delete.php
@@ -1,13 +1,20 @@
 <?php
 //including the database connection file
-include("config.php");
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"])); 
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+
+# Here we establish the connection. Yes, that's all.
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
+
 
 //getting id of the data from url
 $id = $_GET['id'];
 
 //deleting the row from table
-$result = mysqli_query($mysqli, "DELETE FROM users WHERE id=$id");
-
+$result = pg_query($pg_conn, "DELETE FROM menu WHERE item_id=$id");
+//$result = pg_query($pg_conn, "SELECT * FROM menu ");
 //redirecting to the display page (index.php in our case)
 header("Location:index.php");
 ?>
