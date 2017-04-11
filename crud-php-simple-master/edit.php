3833a5137895b7e793331e195085557c74c08dab
Moxxe <moxxew@gmail.com>
2017-04-11 14:49:27 +0500

    admin panel v6.4

--- crud-php-simple-master/edit.php
+++ crud-php-simple-master/edit.php
@@ -1,3 +1,40 @@
+
+
+<html>
+<head>	
+	<title>Edit Data</title>
+</head>
+
+<body>
+	<a href="index.php">Home</a>
+	<br/><br/>
+	
+	<form name="form1" method="post" action="edit.php">
+		<table border="0">
+			<tr> 
+				<td>ID</td>
+				<td><input type="text" name="id" value="<?php echo $id;?>"></td>
+			</tr>
+			<tr> 
+				<td>name</td>
+				<td><input type="text" name="name" value="<?php echo $name;?>"></td>
+			</tr>
+			<tr> 
+				<td>description</td>
+				<td><input type="text" name="desc" value="<?php echo $desc;?>"></td>
+			</tr>
+			<tr> 
+				<td>price</td>
+				<td><input type="text" name="price" value="<?php echo $price;?>"></td>
+			</tr>
+			<tr>
+				<td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
+				<td><input type="submit" name="update" value="Update"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
 <?php
 // including the database connection file
 //including the database connection file
@@ -44,39 +81,3 @@ if(isset($_POST['update']))
 	}
 }
 ?>
-
-<html>
-<head>	
-	<title>Edit Data</title>
-</head>
-
-<body>
-	<a href="index.php">Home</a>
-	<br/><br/>
-	
-	<form name="form1" method="post" action="edit.php">
-		<table border="0">
-			<tr> 
-				<td>ID</td>
-				<td><input type="text" name="id" value="<?php echo $id;?>"></td>
-			</tr>
-			<tr> 
-				<td>name</td>
-				<td><input type="text" name="name" value="<?php echo $name;?>"></td>
-			</tr>
-			<tr> 
-				<td>description</td>
-				<td><input type="text" name="desc" value="<?php echo $desc;?>"></td>
-			</tr>
-			<tr> 
-				<td>price</td>
-				<td><input type="text" name="price" value="<?php echo $price;?>"></td>
-			</tr>
-			<tr>
-				<td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
-				<td><input type="submit" name="update" value="Update"></td>
-			</tr>
-		</table>
-	</form>
-</body>
-</html>
