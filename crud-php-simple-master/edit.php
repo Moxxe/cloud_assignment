0eccbdc46b2b50c141fcfad4be0572df5dbb7804
Moxxe <moxxew@gmail.com>
2017-04-11 15:08:09 +0500

    admin panel v7.1

--- crud-php-simple-master/edit.php
+++ crud-php-simple-master/edit.php
@@ -54,30 +54,3 @@ while($row = pg_fetch_row($result))
 	</form>
 </body>
 </html>
-<?php
-function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["DATABASE_URL"])); 
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-}
-
-# Here we establish the connection. Yes, that's all.
-$pg_conn = pg_connect(pg_connection_string_from_database_url());
-//$result = pg_query($pg_conn, "SELECT * FROM menu ");
-
-if(isset($_POST['update']))
-{	
-
-	//$id = $_POST['id']);
-	$name = $_POST['name'];
-	$desc = $_POST['desc'];
-	$price = $_POST['price'];	
-	
-	
-		$result = pg_query($pg_conn, "UPDATE menu SET item_id='$id',item_name='$name',item_description='$desc',item_price='$price' 
-		 WHERE item_id=$id");
-		
-		//redirectig to the display page. In our case, it is index.php
-		header("Location: index.php");
-	}
-}
-?>
