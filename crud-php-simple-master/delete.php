71f3badce9f52a92fba92442de27295ec37bc37b
Moxxe <moxxew@gmail.com>
2017-04-11 15:14:06 +0500

    admin panel v7.7

--- crud-php-simple-master/delete.php
+++ crud-php-simple-master/delete.php
@@ -13,7 +13,7 @@ $pg_conn = pg_connect(pg_connection_string_from_database_url());
 $id = $_GET['id'];
 
 //deleting the row from table
-$result = pg_query($pg_conn, "DELETE FROM menu WHERE item_id=$id");
+$result = pg_query($pg_conn, "DELETE FROM menu WHERE item_id=2");
 //$result = pg_query($pg_conn, "SELECT * FROM menu ");
 //redirecting to the display page (index.php in our case)
 header("Location:index.php");
