aa9e2bb2cd871ffe6f5867ef79e2171252bfcacb
Moxxe <moxxew@gmail.com>
2017-04-11 12:13:06 +0500

    v9

--- dbconn.php
+++ dbconn.php
@@ -9,11 +9,30 @@ $app->register(new Herrera\Pdo\PdoServiceProvider(),
                    'pdo.password' => $dbopts["pass"]
                )
 );
+$host        = $dbopts["host"];
+   $port        = $dbopts["port"];
+   $dbname      = $dbopts["path"];
+   $credentials = $dbopts["user"] $dbopts["pass"];
 
-if(!$app){
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db){
       echo "Error : Unable to open database\n";
    } else {
       echo "Opened database successfully\n";
    }
 
+   $sql =<<<EOF
+      INSERT INTO menu (item_id,item_name,item_description,item_price)
+      VALUES (4, 'manchurian','okay & okay', 100 );
+
+      
+EOF;
+
+   $ret = pg_query($db, $sql);
+   if(!$ret){
+      echo pg_last_error($db);
+   } else {
+      echo "Records created successfully\n";
+   }
+   pg_close($db);
 ?>
