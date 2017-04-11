604c700589f9c138f6ea92f56a8ed3c368ec3cb2
Moxxe <moxxew@gmail.com>
2017-04-11 15:49:04 +0500

    admin panel v9

--- crud-php-simple-master/edit.php
+++ crud-php-simple-master/edit.php
@@ -10,17 +10,13 @@ if(isset($_POST['update']))
 	
 
 	
-		//$result = pg_query($pg_conn, "UPDATE menu SET item_name='$name',item_description='$desc',item_price='$price' 
-		 //WHERE item_id=$id");
+		$result = pg_query($pg_conn, "UPDATE menu SET item_name='$name',item_description='$desc',item_price='$price' 
+		 WHERE item_id=$id");
 		
 		//redirectig to the display page. In our case, it is index.php
 		header("Location: index.php");
 	}
-	else
-{
-	echo "problem is here";
-	header("Location: index.php");
-}
+	
 
 //header("Location: index.php");
 
