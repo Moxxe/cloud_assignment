554f163e825b894ca446493376b7e6dd0d89885b
Moxxe <moxxew@gmail.com>
2017-04-11 15:38:03 +0500

    admin panel v7.1

--- crud-php-simple-master/edit_2.php
+++ crud-php-simple-master/edit_2.php
@@ -1,12 +1,7 @@
 <?php
 
-# Here we establish the connection. Yes, that's all.
-//$pg_conn = pg_connect(pg_connection_string_from_database_url());
-//$result = pg_query($pg_conn, "SELECT * FROM menu ");
 
-
-if(isset($_POST['update']))
-{	
+	
 
 	//$id = $_POST['id']);
 	$name = $_POST['name'];
