6d01b68c52929d18efa13356fc9befce141d41a6
Moxxe <moxxew@gmail.com>
2017-04-11 11:24:57 +0500

    Composer updated

---
+++ dbconn.php
@@ -0,0 +1,27 @@
+<?php
+   $host        = "host=127.0.0.1";
+   $port        = "port=5432";
+   $dbname      = "dbname=postgresql-curly-12391";
+   $credentials = "user= password=";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db){
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
+
+   $sql =<<<EOF
+      INSERT INTO menu (item_id,item_name,item_description,item_price)
+      VALUES (4, 'manchurian', 'okay',50);
+
+EOF;
+
+   $ret = pg_query($db, $sql);
+   if(!$ret){
+      echo pg_last_error($db);
+   } else {
+      echo "Records created successfully\n";
+   }
+   pg_close($db);
+?>
