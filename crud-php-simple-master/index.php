45804c33e6f1b69618e529daad8b9bcc3a24e4f2
Moxxe <moxxew@gmail.com>
2017-04-11 14:13:02 +0500

    v4 admin_panel

--- crud-php-simple-master/index.php
+++ crud-php-simple-master/index.php
@@ -4,7 +4,11 @@ include_once("config.php");
 
 //fetching data in descending order (lastest entry first)
 //$result = mysql_query("SELECT * FROM users ORDER BY id DESC"); // mysql_query is deprecated
-$result = mysqli_query($mysqli, "SELECT * FROM menu"); // using mysqli_query instead
+$result = mysqli_query($mysqli, "SELECT * FROM menu"); 
+if(!$result)
+{
+	echo "unsucessful";
+}// using mysqli_query instead
 ?>
 
 <html>
