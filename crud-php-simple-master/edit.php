e128c1e7e0ee7167978f2bc8f732e4f4fafdf857
Moxxe <moxxew@gmail.com>
2017-04-11 15:45:01 +0500

    admin panel v7.8

--- crud-php-simple-master/edit.php
+++ crud-php-simple-master/edit.php
@@ -3,11 +3,11 @@ if(isset($_POST['update']))
 {	
 
 	//$id = $_POST['id']);
-	//$name = $_POST['name'];
+	$name = $_POST['name'];
 	//$desc = $_POST['desc'];
 	//$price = $_POST['price'];	
 	//echo(name);
-	echo ("no problem here");
+	
 
 	
 		//$result = pg_query($pg_conn, "UPDATE menu SET item_name='$name',item_description='$desc',item_price='$price' 
