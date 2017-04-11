ce634803c06887c5028ffe0f1aacb27e31d0b699
Moxxe <moxxew@gmail.com>
2017-04-11 11:53:33 +0500

    dbconn v2

--- dbconn.php
+++ dbconn.php
@@ -1,53 +1,13 @@
 <?php
-$url = parse_url(getenv("DATABASE_URL"));
-
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
-
-//Then change your pgsql entry in that same file to be the following:
-
-'pgsql' => array(
-    'driver'   => 'pgsql',
-    'host'     => $host,
-    'database' => $database,
-    'username' => $username,
-    'password' => $password,
-    'charset'  => 'utf8',
-    'prefix'   => '',
-    'schema'   => 'public',
-),
 
 
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+               array(
+                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+                   'pdo.username' => $dbopts["user"],
+                   'pdo.password' => $dbopts["pass"]
+               )
+);
 
-
-
-
-
-   
-   $port        = "port=5432";
-  
-   $credentials = "user= $username password=$password";
-
-   $db = pg_connect( "$host $port $dbname $credentials"  );
-   if(!$db){
-      echo "Error : Unable to open database\n";
-   } else {
-      echo "Opened database successfully\n";
-   }
-
-   $sql =<<<EOF
-      INSERT INTO menu (item_id,item_name,item_description,item_price)
-      VALUES (4, 'manchurian', 'okay',50);
-
-EOF;
-
-   $ret = pg_query($db, $sql);
-   if(!$ret){
-      echo pg_last_error($db);
-   } else {
-      echo "Records created successfully\n";
-   }
-   pg_close($db);
 ?>
