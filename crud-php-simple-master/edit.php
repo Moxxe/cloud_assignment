e05d5dbfdfad040b188bbdbf4bc44dc04fdfafcf
Moxxe <moxxew@gmail.com>
2017-04-11 18:33:58 +0500

    admin panel v9.0.2

--- crud-php-simple-master/edit.php
+++ crud-php-simple-master/edit.php
@@ -34,6 +34,8 @@ function pg_connection_string_from_database_url() {
 # Here we establish the connection. Yes, that's all.
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 //selecting data associated with this particular id
+$result = pg_query($pg_conn,"UPDATE menu SET  item_price='87'
+		 WHERE item_id=$id");
 $result = $result = pg_query($pg_conn, "SELECT * FROM menu WHERE item_id=$id");
 
 while($row = pg_fetch_row($result))
