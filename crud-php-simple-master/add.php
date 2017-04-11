b00f52a322e2e162821bb4115ac2ec4b338290d0
Moxxe <moxxew@gmail.com>
2017-04-11 16:49:52 +0500

    admin panel v9.0.1

--- crud-php-simple-master/add.php
+++ crud-php-simple-master/add.php
@@ -15,17 +15,17 @@ function pg_connection_string_from_database_url() {
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
 if(isset($_POST['Submit'])) {
-	echo $_POST['id'];
-	$id = mysqli_real_escape_string($mysqli, $_POST['id']);
-	$name = mysqli_real_escape_string($mysqli, $_POST['name']);
-	$desc = mysqli_real_escape_string($mysqli, $_POST['desc']);
-	$price = mysqli_real_escape_string($mysqli, $_POST['price']);
+	//echo $_POST['id'];
+	$id =  $_POST['id'];
+	$name = $_POST['name'];
+	$desc = , $_POST['desc'];
+	$price =$_POST['price'];
 		
 	
 		// if all the fields are filled (not empty) 
 			
 		//insert data to database	
-		$result = pg_query($pg_conn, "INSERT INTO menu (item_id,item_name,item_description,item_price) VALUES('5','burger','dghjfk','56')");
+		$result = pg_query($pg_conn, "INSERT INTO menu (item_id,item_name,item_description,item_price) VALUES($id,'burger','dghjfk','56')");
 		
 		//display success message
 		echo "<font color='green'>Data added successfully.";
