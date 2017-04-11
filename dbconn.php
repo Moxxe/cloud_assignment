8e68bf3b9101ab0a8f666665c2619de794849bc2
Moxxe <moxxew@gmail.com>
2017-04-11 13:44:54 +0500

    v11

--- dbconn.php
+++ dbconn.php
@@ -2,7 +2,7 @@
 # This function reads your DATABASE_URL config var and returns a connection
 # string suitable for pg_connect. Put this in your app.
 function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["DATABASE_URL"]));
+  extract(parse_url($_ENV["DATABASE_URL"])); 
   return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
 
@@ -10,7 +10,7 @@ function pg_connection_string_from_database_url() {
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
 # Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT item_name FROM menu ");
+$result = pg_query($pg_conn, "SELECT * FROM menu ");
 print "<pre>\n";
 if (!pg_num_rows($result)) {
   print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
