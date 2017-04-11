e9bb258ad689016a8d7f7d0dc700a3da0a31a68b
Moxxe <moxxew@gmail.com>
2017-04-11 15:57:22 +0500

    admin panel v7.7

--- crud-php-simple-master/edit.php
+++ crud-php-simple-master/edit.php
@@ -9,10 +9,7 @@ if(isset($_POST['update']))
 	//echo(name);
 	
 
-	function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["DATABASE_URL"])); 
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-}
+	
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
 		$result = pg_query($pg_conn,"UPDATE menu SET item_name='$name',item_description='$desc',item_price='$price' 
