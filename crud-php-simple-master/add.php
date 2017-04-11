abd5201c37e3ad4f51631a66457a1ccf81b319b9
Moxxe <moxxew@gmail.com>
2017-04-11 16:15:21 +0500

    admin panel v9.0.1

--- crud-php-simple-master/add.php
+++ crud-php-simple-master/add.php
@@ -6,40 +6,30 @@
 <body>
 <?php
 //including the database connection file
-include_once("config.php");
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"])); 
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+
+# Here we establish the connection. Yes, that's all.
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
 
 if(isset($_POST['Submit'])) {	
+	$id = mysqli_real_escape_string($mysqli, $_POST['id']);
 	$name = mysqli_real_escape_string($mysqli, $_POST['name']);
-	$age = mysqli_real_escape_string($mysqli, $_POST['age']);
-	$email = mysqli_real_escape_string($mysqli, $_POST['email']);
+	$desc = mysqli_real_escape_string($mysqli, $_POST['desc']);
+	$price = mysqli_real_escape_string($mysqli, $_POST['price']);
 		
-	// checking empty fields
-	if(empty($name) || empty($age) || empty($email)) {
-				
-		if(empty($name)) {
-			echo "<font color='red'>Name field is empty.</font><br/>";
-		}
-		
-		if(empty($age)) {
-			echo "<font color='red'>Age field is empty.</font><br/>";
-		}
-		
-		if(empty($email)) {
-			echo "<font color='red'>Email field is empty.</font><br/>";
-		}
-		
-		//link to the previous page
-		echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
-	} else { 
+	
 		// if all the fields are filled (not empty) 
 			
 		//insert data to database	
-		$result = mysqli_query($mysqli, "INSERT INTO users(name,age,email) VALUES('$name','$age','$email')");
+		$result = pg_query($pg_conn, "INSERT INTO menu (item_id,item_name,item_description,item_price) VALUES('$id','$name','$desc','$price')");
 		
 		//display success message
 		echo "<font color='green'>Data added successfully.";
 		echo "<br/><a href='index.php'>View Result</a>";
-	}
+	
 }
 ?>
 </body>
