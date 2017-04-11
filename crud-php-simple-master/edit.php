12c0710873e16d005c349ee58db745102169f5cd
Moxxe <moxxew@gmail.com>
2017-04-11 14:41:45 +0500

    admin panel v5

--- crud-php-simple-master/edit.php
+++ crud-php-simple-master/edit.php
@@ -1,6 +1,14 @@
 <?php
 // including the database connection file
-include_once("config.php");
+//including the database connection file
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"])); 
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+
+# Here we establish the connection. Yes, that's all.
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
+//$result = pg_query($pg_conn, "SELECT * FROM menu ");
 
 if(isset($_POST['update']))
 {	
@@ -8,9 +16,10 @@ if(isset($_POST['update']))
 	$id = mysqli_real_escape_string($mysqli, $_POST['id']);
 	
 	$name = mysqli_real_escape_string($mysqli, $_POST['name']);
-	$age = mysqli_real_escape_string($mysqli, $_POST['age']);
-	$email = mysqli_real_escape_string($mysqli, $_POST['email']);	
+	$desc = mysqli_real_escape_string($mysqli, $_POST['age']);
+	$price = mysqli_real_escape_string($mysqli, $_POST['email']);	
 	
+	/*
 	// checking empty fields
 	if(empty($name) || empty($age) || empty($email)) {	
 			
@@ -25,9 +34,10 @@ if(isset($_POST['update']))
 		if(empty($email)) {
 			echo "<font color='red'>Email field is empty.</font><br/>";
 		}		
-	} else {	
+	} else {*/	
 		//updating the table
-		$result = mysqli_query($mysqli, "UPDATE users SET name='$name',age='$age',email='$email' WHERE id=$id");
+		$result = pg_query($mysqli, "UPDATE menu SET item_id='$id',item_name='$name',item_description='$desc',item_price='$price' 
+		 WHERE item_id=$id");
 		
 		//redirectig to the display page. In our case, it is index.php
 		header("Location: index.php");
@@ -39,13 +49,14 @@ if(isset($_POST['update']))
 $id = $_GET['id'];
 
 //selecting data associated with this particular id
-$result = mysqli_query($mysqli, "SELECT * FROM users WHERE id=$id");
+$result = $result = pg_query($pg_conn, "SELECT * FROM menu WHERE item_id=$id");
 
 while($res = mysqli_fetch_array($result))
 {
-	$name = $res['name'];
-	$age = $res['age'];
-	$email = $res['email'];
+	$id = $res[0];
+	$name = $res[1];
+	$desc = $res[2];
+	$price = $res[3];
 }
 ?>
 <html>
@@ -60,16 +71,20 @@ while($res = mysqli_fetch_array($result))
 	<form name="form1" method="post" action="edit.php">
 		<table border="0">
 			<tr> 
-				<td>Name</td>
+				<td>ID</td>
+				<td><input type="text" name="id" value="<?php echo $id;?>"></td>
+			</tr>
+			<tr> 
+				<td>name</td>
 				<td><input type="text" name="name" value="<?php echo $name;?>"></td>
 			</tr>
 			<tr> 
-				<td>Age</td>
-				<td><input type="text" name="age" value="<?php echo $age;?>"></td>
+				<td>description</td>
+				<td><input type="text" name="desc" value="<?php echo $desc;?>"></td>
 			</tr>
 			<tr> 
-				<td>Email</td>
-				<td><input type="text" name="email" value="<?php echo $email;?>"></td>
+				<td>price</td>
+				<td><input type="text" name="price" value="<?php echo $price;?>"></td>
 			</tr>
 			<tr>
 				<td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
