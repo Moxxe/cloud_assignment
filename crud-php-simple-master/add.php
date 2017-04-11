36bde70fe155bfce161678e784a044af87d33467
Moxxe <moxxew@gmail.com>
2017-04-11 16:52:54 +0500

    admin panel v9.0.1

--- crud-php-simple-master/add.php
+++ crud-php-simple-master/add.php
@@ -25,11 +25,11 @@ if(isset($_POST['Submit'])) {
 		// if all the fields are filled (not empty) 
 			
 		//insert data to database	
-		$result = pg_query($pg_conn, "INSERT INTO menu (item_id,item_name,item_description,item_price) VALUES('$id','burger','dghjfk','56')");
+		$result = pg_query($pg_conn, "INSERT INTO menu (item_id,item_name,item_description,item_price) VALUES("$id",'burger','dghjfk','56')");
 		
 		//display success message
 		echo "<font color='green'>Data added successfully.";
-		echo $name;
+		
 		echo "<br/><a href='index.php'>View Result</a>";
 	
 }
