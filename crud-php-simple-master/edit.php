76a77930180d2f8ccb1896e520e3f9f224878894
Moxxe <moxxew@gmail.com>
2017-04-12 11:02:45 +0500

    version beta

--- crud-php-simple-master/edit.php
+++ crud-php-simple-master/edit.php
@@ -16,7 +16,7 @@ if(isset($_POST['update']))
 		
 
 		$result = pg_query($pg_conn,"UPDATE menu SET item_price='$price'
-		 WHERE item_id=$id");
+		 WHERE item_id='$id'");
 		
 		//redirectig to the display page. In our case, it is index.php
 		header("Location: index.php");
